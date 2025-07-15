b393dc9fccc3bb69053c667a2a70e704c68c5793
Siddhesh-Mavrick <siddheshbhise04@gmail.com>
2025-07-15 17:57:53 +0530

    new route sid

--- routes/testRouter.js
+++ routes/testRouter.js
@@ -10,5 +10,13 @@ router.get('/hii', (req, rep) =>{
     return rep.send("<h1>heeloo</h1>");
 })
 
+router.get('/git-testing', (req,res)=> {
+    return res.json({
+        message : "able to understand git",
+        success : true,
+        name : "tushar"
+    })
+})
+
 //export
 module.exports = router
